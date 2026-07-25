2f55e000b239e3523de5d1bfaf1c2e4916a74e52
Vidya Gurram <gurramvidya@gmail.com>
2026-07-25 17:17:47 -0500

    structuring response using Pydantic

--- main.py
+++ main.py
@@ -6,9 +6,21 @@ from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from tavily import TavilyClient
 from langchain_tavily import TavilySearch
+from typing import List
+from pydantic import BaseModel, Field
 import os
 
 load_dotenv(override=True)
+
+class Source(BaseModel):
+    """Schema for the source used by an agent"""
+    url:str=Field(description="The url of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for the Agent response with answer and sources"""
+    answer:str=Field(description="The agents answer to the query")
+    sources:List[Source]=Field(default_factory=list, description="List of sources used to generate the answer")
+
 
 
 # Using tavilyClient
@@ -30,7 +42,7 @@ load_dotenv(override=True)
 llm = ChatOpenAI(model="gpt-5")
 #tools = [search]
 tools = [TavilySearch()]
-agent = create_agent(model = llm, tools=tools)
+agent = create_agent(model = llm, tools=tools, response_format=AgentResponse)
 
 def main():
     print("Hello from langchain-documentationhelper!")
